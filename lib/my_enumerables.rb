795be44f2d50bc57676f71a0203adb58325203b2
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-19 18:50:43 -0500

    Complete Challenges

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,111 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield self[i], i
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    i = 0
+    array = []
+    while i < length
+      if yield self[i]
+        array << self[i]
+      end
+      i +=1
+    end
+    array
+  end
+  def my_all?
+    i = 0
+    opc = true
+    while i < length
+      unless yield(self[i])
+        opc = false
+      end
+      i +=1
+    end
+    opc
+  end
+  def my_any?
+    i = 0
+    opc = false
+    while i < length
+      if yield(self[i])
+        opc = true
+        i = length
+      end
+      i +=1
+    end
+    opc
+  end
+  def my_none?
+    i = 0
+    opc = true
+    while i < length
+      if yield(self[i])
+        opc = false
+      end
+      i +=1
+    end
+    opc
+  end
+  def my_count(*item)
+    if block_given?
+      i = 0
+      cont = 0
+      while i < length
+        if yield(self[i])
+          cont += 1
+        end
+        i +=1
+      end
+      cont
+    else
+      if item == []
+        length
+      else
+        i = 0
+        cont = 0
+        while i < length
+          if self[i] == item[0]
+            cont += 1
+          end
+          i +=1
+        end
+        cont
+      end
+    end
+  end
+  def my_map
+    i = 0
+    array = []
+    while i < length
+      array << yield(self[i])
+      i += 1
+    end
+    array
+  end
+  def my_inject(*index)
+    if block_given?
+      if index == []
+        i = 1
+        result = self.to_a[0]
+      else
+        result = index[0]
+        i = 0
+      end
+      while i < self.to_a.length
+        result =  yield(result,self.to_a[i])
+        i += 1
+      end
+      result
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +114,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    while i < length
+      yield  self[i]
+      i += 1
+    end
+    self
+  end
 end
